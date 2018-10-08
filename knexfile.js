a2f1081a4d9d510acb395381e0e443b8fd3c5236
Kris Kamikawa <kris.kamikawa@gmail.com>
2018-10-08 11:23:15 -0600

    Create knex file

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+
+  development: {
+    client: "pg",
+    connection: "postgresql://localhost:tech-books-database"
+  },
+
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_url + "ssl=true"
+  }
+
+};
